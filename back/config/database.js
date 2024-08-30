42c8c0ba3dcd55b6f79de8cc74f3ab5049f1c3a3
3AlblA3 <162109672+3AlblA3@users.noreply.github.com>
2024-08-30 10:36:18 +0200

    add front files

--- back/config/database.js
+++ back/config/database.js
@@ -1,14 +1,13 @@
 require('dotenv').config();
 const { Sequelize } = require('sequelize');
 
-
 const sequelize = new Sequelize(
     process.env.DB_NAME, process.env.DB_USER, process.env.DB_PASSWORD, {
     host: process.env.DB_HOST,
     dialect: 'mysql',
     dialectModule: require('mysql2'), 
     logging: false,
-    define: {
+    define:{
         timestamps: true, 
         underscored: true, 
         paranoid: true,
--- back/routes/routePostLike.js
+++ back/routes/routePostLike.js
@@ -3,10 +3,10 @@ const router = express.Router();
 const ctrlPostLike = require('../controllers/ctrlPostLike');
 const auth = require('../middlewares/auth');
 const checkPostLike = require('../middlewares/checkPostLike');
-
-router.get('/', ctrlPostLike.getAllPostLikes);
-router.post('/', auth, ctrlPostLike.createPostLike);
+;
 router.get('/:id', ctrlPostLike.getOnePostLike);
 router.delete('/:id', auth, checkPostLike, ctrlPostLike.deletePostLike);
 
+router.get('/', ctrlPostLike.getAllPostLikes);
+router.post('/', auth, ctrlPostLike.createPostLike)
 module.exports = router;
